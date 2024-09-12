4711441f228f8da1df7cf9ebc739ddd7792dae98
Ariel Sanchez <freska_96@hotmail.com>
2024-09-12 17:26:58 -0600

    feat: eliminar un evento - backend terminado

--- controllers/events.js
+++ controllers/events.js
@@ -49,7 +49,7 @@ const actualizarEvento = async (req, res = response) => {
         const evento = await Evento.findById(eventoId);
 
         if (!evento) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
@@ -83,12 +83,40 @@ const actualizarEvento = async (req, res = response) => {
     };
 };
 
-const eliminarEvento = (req, res = response) => {
+const eliminarEvento = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    });
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        };
+
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene el privilegio de eliminar este evento'
+            });
+        };
+
+        await Evento.findByIdAndDelete(eventoId);
+
+        res.json({ ok: true });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    };
 
 };
 
@@ -97,4 +125,4 @@ module.exports = {
     crearEvento,
     actualizarEvento,
     eliminarEvento
-}
+};
