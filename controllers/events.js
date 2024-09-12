78a037d3c3647bfc49ce504cfe99198b20433569
Ariel Sanchez <freska_96@hotmail.com>
2024-09-12 05:48:00 -0600

    feat: grabar evento en la base de datos de MongoDB

--- controllers/events.js
+++ controllers/events.js
@@ -1,5 +1,6 @@
 
 const { response } = require('express');
+const Evento = require('../models/Evento');
 
 const getEventos = (req, res = response) => {
 
@@ -10,15 +11,28 @@ const getEventos = (req, res = response) => {
 
 };
 
-const crearEvento = (req, res = response) => {
+const crearEvento = async (req, res = response) => {
 
-    // verificar que tenga el evento
-    console.log(req.body);
+    const evento = new Evento(req.body);
 
-    res.json({
-        ok: true,
-        msg: 'crearEvento'
-    });
+    try {
+
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save()
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador"
+        });
+    };
 
 };
 
