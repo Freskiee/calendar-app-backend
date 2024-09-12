0c3ee182b3b56a02689919cf9d848967ed2a8af2
Ariel Sanchez <freska_96@hotmail.com>
2024-09-12 05:15:04 -0600

    feat: creando el modelo del Evento

--- controllers/events.js
+++ controllers/events.js
@@ -12,6 +12,9 @@ const getEventos = (req, res = response) => {
 
 const crearEvento = (req, res = response) => {
 
+    // verificar que tenga el evento
+    console.log(req.body);
+
     res.json({
         ok: true,
         msg: 'crearEvento'
